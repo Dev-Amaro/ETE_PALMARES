eb5afd596a11f04991f205d433f0ebc841160265
Dev-Amaro <joao.guilherme1827@gmail.com>
2022-05-26 14:17:52 -0300

    beck-end

--- back.js
+++ back.js
@@ -1,7 +1,33 @@
-function login(){
-    var user = document.getElementById('user');
-    var pass = document.getElementById('pass');
-    if(user == "oi" && pass == "oi"){
-        window.l
+var usuarios = [
+    {"login": "sergio10", "senha": "sergio"},
+    {"login": "valmir", "senha": "valmir"},
+    {"login": "alex", "senha": "alex"},
+    {"login": "luana", "senha": "luana"},
+    {"login": "otavio", "senha": "otavio"},
+    {"login": "andreia", "senha": "andreia"},
+    {"login": "mariajose", "senha": "mariajose"},
+    {"login": "esdras", "senha": "esdras"},
+    {"login": "lauro", "senha": "lauro"},
+    {"login": "alberto", "senha": "alberto"},
+    {"login": "marcelo", "senha": "marcelo"},
+    {"login": "carlos", "senha": "carlos"},
+];
+function novaAba(url){
+    var win = window.open(url, '_blank');
+    win.focus();
+}
+
+function Login() {
+    var usuario = document.getElementsByName('email')[0].value.toLowerCase();
+    var senha = document.getElementsByName('pass')[0].value;
+
+    for (var u in usuarios) {
+        var us = usuarios[u];
+        if (us.login === usuario && us.senha === senha) {
+            novaAba('prof.html');
+            return true;
+        }
     }
+    alert("Dados incorretos, tente novamente.");
+    return false;
 }
