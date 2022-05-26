2ae929436c7365508644cc49b14e91160a28d0e8
Dev-Amaro <joao.guilherme1827@gmail.com>
2022-05-26 11:22:44 -0300

    criacao da area professor

---
+++ back.js
@@ -0,0 +1,7 @@
+function login(){
+    var user = document.getElementById('user');
+    var pass = document.getElementById('pass');
+    if(user == "oi" && pass == "oi"){
+        window.l
+    }
+}
